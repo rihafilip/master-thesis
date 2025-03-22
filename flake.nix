51daa5ab96399404d38669bdd97ca733b8e15a75
Filip Riha <rihafili@fit.cvut.cz>
2025-03-22 20:26:16 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+	  pkgs = nixpkgs.legacyPackages."x86_64-linux";
+	in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        name = "LaTex";
+        buildInputs = with pkgs; [
+          texlive.combined.scheme-full
+          latexminted
+        ];
+      };
+    };
+}
+
